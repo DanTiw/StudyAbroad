02e7e1d721950ba4cc78f2f67dfa35be5c6df4b5
DanTiw <danishtiwari2204@gmail.com>
2024-07-28 15:02:35 +0530

    Finalized

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Merriweather } from "next/font/google";
 import "./globals.css";
 import Navbar from "./navbar";
+
+import { Analytics } from '@vercel/analytics/react';
 const inter = Merriweather({ subsets: ["latin"], weight:  ["700" ] });
 
 export const metadata: Metadata = {
@@ -20,6 +22,7 @@ export default function RootLayout({
       <div className="min-h-screen bg-gradient-to-t from-cyan-500  to-pink-500 text-black-500 text-white">
         <Navbar/>
         {children}
+        <Analytics />
       </div>  
         </body>
     </html>
